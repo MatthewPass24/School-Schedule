799f28102ba8e5dcd19896984cbad5edd70e97c7
MatthewPass24 <matthewpass24@gmail.com>
2024-10-28 14:58:17 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    
+    const scheduleUrl = 'https://api.npoint.io/b933780fce6871f9856e'
+    
+    const bellSchedule = {
+        1: { start: '8:24 AM', end: '9:31 AM'},
+        2: { start: '9:36 AM', end: '10:43 AM'},
+        3: { start: '8:24 AM', end: '11:55 AM'},
+        4: { start: '12:00 PM', end: '12:35 PM'},
+        5: { start: '1:41 PM', end: '1:48 PM'},
+        6: { start: '1:53 PM', end: '3:00 PM'},
+    }
+    btn.on('click', function () {
+        const selectedDay = $('#dayInput').val().trim().toUpperCase();
+    })
+
+})
