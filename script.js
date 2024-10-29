e7d0d810204311313f304b4c7075c0590b0cb9a0
MatthewPass24 <matthewpass24@gmail.com>
2024-10-29 11:54:53 -0400

    added alert and started the filter function

--- script.js
+++ script.js
@@ -10,8 +10,32 @@ $(document).ready(function () {
         5: { start: '1:41 PM', end: '1:48 PM'},
         6: { start: '1:53 PM', end: '3:00 PM'},
     }
-    btn.on('click', function () {
-        const selectedDay = $('#dayInput').val().trim().toUpperCase();
+$('#submitDay').on('click', function (event) {
+        let selectedDay = $('#dayInput').val().toUpperCase();
+        
+        if (selectedDay != "A" && selectedDay != "B" && selectedDay != "C" && selectedDay != "D" && selectedDay != "E" && selectedDay != "F" && selectedDay != "G") {
+            alert("Please enter a valid letter from A to G");
+        } else {
+                $.ajax({
+                    type: "GET",
+                    url: "https://api.npoint.io/b933780fce6871f9856e",
+                    success: function (data) {
+                        let schedule = data.schedule.filter(classItem =>
+                            classItem.days.includes(selectedDay)
+                        )
+                        let htmlString = '<tr>'
+                        schedule.forEach(classItem => {
+                            htmlString += `<td> ${classItem.period}</td>
+                                            <td> ${classItem.time}</td>
+                                            <td> ${classItem.class}</td>`
+                                            
+                            console.log(htmlString)
+                        }
+                        )
+                    }
+                });
+
+        }
     })
 
 })
