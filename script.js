883b290dd678bcd303212221aec21e278c8beefa
MatthewPass24 <matthewpass24@gmail.com>
2024-10-31 13:49:02 -0400

    added different classes for each letter day

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ $(document).ready(function () {
         5: { start: '1:41 PM', end: '1:48 PM'},
         6: { start: '1:53 PM', end: '3:00 PM'},
     }
+    const dailySchedule = {
+        A: [1, 2, 3, 'Lunch', 5, 6],
+        B: [4, 1, 2, 'Lunch', 7, 5],
+        C: [3, 4, 1, 'Lunch', 6, 7],
+        D: [2, 3, 4, 'Lunch', 5, 6],
+        E: [1, 2, 3, 'Lunch', 7, 5],
+        F: [4, 1, 2, 'Lunch', 6, 7],
+        G: [3, 4, 7, 'Lunch', 5, 6],
+
+    }
 $('#submitDay').on('click', function (event) {
         let selectedDay = $('#dayInput').val().toUpperCase();
         
@@ -20,18 +30,23 @@ $('#submitDay').on('click', function (event) {
                     type: "GET",
                     url: "https://api.npoint.io/b933780fce6871f9856e",
                     success: function (data) {
+                        const daySchedule = dailySchedule[selectedDay];
                         let schedule = data.schedule.filter(classItem =>
                             classItem.days.includes(selectedDay)
                         )
+                        $('#scheduleList').empty();
                         let htmlString = '<tr>'
-                        schedule.forEach(classItem => {
+                        schedule.forEach((classItem) => {
                             htmlString += `<td> ${classItem.period}</td>
                                             <td> ${classItem.time}</td>
-                                            <td> ${classItem.class}</td>`
-                                            
-                            console.log(htmlString)
-                        }
-                        )
+                                            <td> ${classItem.class}</td>
+                                            <td> ${classItem.teacher}</td>
+                                            <td> ${classItem.room}</td> `;
+                            htmlString += "</tr>";
+                        })
+
+                        $("#scheduleList").append(htmlString);
+                        let bellScheduleIndex = 1 
                     }
                 });
 
