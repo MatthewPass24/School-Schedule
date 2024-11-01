64b68887886bcc31db742ae49c5b70f88ee878e1
MatthewPass24 <matthewpass24@gmail.com>
2024-11-01 14:46:07 -0400

    added the correct order and styling

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 $(document).ready(function () {
     
     const scheduleUrl = 'https://api.npoint.io/b933780fce6871f9856e'
-    
-    const bellSchedule = {
-        1: { start: '8:24 AM', end: '9:31 AM'},
-        2: { start: '9:36 AM', end: '10:43 AM'},
-        3: { start: '8:24 AM', end: '11:55 AM'},
-        4: { start: '12:00 PM', end: '12:35 PM'},
-        5: { start: '1:41 PM', end: '1:48 PM'},
-        6: { start: '1:53 PM', end: '3:00 PM'},
-    }
+
     const dailySchedule = {
         A: [1, 2, 3, 'Lunch', 5, 6],
         B: [4, 1, 2, 'Lunch', 7, 5],
@@ -20,37 +12,53 @@ $(document).ready(function () {
         G: [3, 4, 7, 'Lunch', 5, 6],
 
     }
-$('#submitDay').on('click', function (event) {
+    $('#submitDay').on('click', function (event) {
         let selectedDay = $('#dayInput').val().toUpperCase();
-        
+
         if (selectedDay != "A" && selectedDay != "B" && selectedDay != "C" && selectedDay != "D" && selectedDay != "E" && selectedDay != "F" && selectedDay != "G") {
             alert("Please enter a valid letter from A to G");
         } else {
-                $.ajax({
-                    type: "GET",
-                    url: "https://api.npoint.io/b933780fce6871f9856e",
-                    success: function (data) {
-                        const daySchedule = dailySchedule[selectedDay];
-                        let schedule = data.schedule.filter(classItem =>
-                            classItem.days.includes(selectedDay)
-                        )
-                        $('#scheduleList').empty();
-                        let htmlString = '<tr>'
-                        schedule.forEach((classItem) => {
-                            htmlString += `<td> ${classItem.period}</td>
-                                            <td> ${classItem.time}</td>
-                                            <td> ${classItem.class}</td>
-                                            <td> ${classItem.teacher}</td>
-                                            <td> ${classItem.room}</td> `;
-                            htmlString += "</tr>";
-                        })
+            $.ajax({
+                type: "GET",
+                url: "https://api.npoint.io/b933780fce6871f9856e",
+                success: function (data) {
+                    let schedule = data.schedule.filter(classItem =>
+                        classItem.days.includes(selectedDay)
+                    );
 
-                        $("#scheduleList").append(htmlString);
-                        let bellScheduleIndex = 1 
-                    }
-                });
 
+                    $('#scheduleList').empty();
+                    let htmlString = '';
+
+                    // Iterate over the daily schedule and build the output
+                    dailySchedule[selectedDay].forEach(period => {
+                        if (period === 'Lunch') {
+                            htmlString += `<tr>
+                                <td>${period}</td>
+                                <td>Lunch</td>
+                                <td>N/A</td>
+                                <td>Cafe</td>
+                            </tr>`;
+                        } else {
+                            let classItem = schedule.find(item => item.period === period);
+                            if (classItem) {
+                                htmlString += `<tr>
+                                    <td>${classItem.period}</td>
+                                    <td>${classItem.class}</td>
+                                    <td>${classItem.teacher}</td>
+                                    <td>${classItem.room}</td>
+                                </tr>`;
+                            }
+                        }
+                    });
+
+                    $("#scheduleList").append(htmlString);
+                },
+                error: function () {
+                    alert("Failed to fetch the schedule. Please try again later.");
+                }
+            });
         }
-    })
+    });
 
 })
